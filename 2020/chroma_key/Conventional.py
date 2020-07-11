76c46bc2b00ef4b4d2fa4176c7ca13170eae77b6
bjsh1290576@gn.iwasaki.ac.jp <bjsh1290576@gn.iwasaki.ac.jp>
2020-07-11 19:55:35 +0900

    dekita!

--- 2020/chroma_key/Conventional.py
+++ 2020/chroma_key/Conventional.py
@@ -6,38 +6,36 @@ import os
 import time
 
 
-video_capture = cv2.VideoCapture(0)
-
-embedded = cv2.imread("./image/weather.png")
-
-video_capture.set(cv2.CAP_PROP_FPS, 30)
-
-width = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-def ExtractionColor(frame):
+def ExtractionColor(frame, back_image):
+    back_image = cv2.resize(back_image, dsize=(800, 800))
+    frame = cv2.resize(frame, dsize=(800, 800))
+    
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     hsv_min = np.array([30, 64, 0])
     hsv_max = np.array([90, 255, 255])
     
-    mask = cv2.inRange(hsv, hsv_min, hsv_max)
-    mask_process = cv2.bitwise_and(frame, frame, mask = mask)
-    mask_not = cv2.bitwise_not(mask)
-    frame_cut = cv2.bitwise_and(frame, frame, mask= mask_not)
-    
-    ChromaKey = cv2.bitwise_or(mask_process, frame_cut)
+    src_image_mask = cv2.inRange(hsv, hsv_min, hsv_max)
+    object_image = cv2.bitwise_and(back_image, back_image, mask = src_image_mask)
+    extraction = cv2.bitwise_not(src_image_mask)
+    embedded = cv2.bitwise_and(frame, frame, mask = extraction)
+    ChromaKey = cv2.bitwise_or(object_image, embedded)
     
     return ChromaKey
 
 
 
 def main():
+    video_capture = cv2.VideoCapture(0)
+
+    back_image = cv2.imread("./image/weather.png")
+
+    video_capture.set(cv2.CAP_PROP_FPS, 30)
+    
     while True:
         ret, frame = video_capture.read()
         
-        #ExtractionColor(frame)
-
-        cv2.imshow("Video", ExtractionColor(frame))
+        cv2.imshow("ChromaKey", ExtractionColor(frame, back_image))
+    
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
 
